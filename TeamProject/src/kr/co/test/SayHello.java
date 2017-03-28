63edb2f02c2a101dcd9dcda5266c8f7cc34a1545
ji2unjang <ji2unjang@gmail.com>
2017-03-28 11:49:41 +0900

    Update SayHello.java
    pulling test

--- TeamProject/src/kr/co/test/SayHello.java
+++ TeamProject/src/kr/co/test/SayHello.java
@@ -5,5 +5,6 @@ public class SayHello {
 		System.out.println("Test for Remote Repository.");
 		System.out.println("Say Hello from local - master branch.");
 		System.out.println("Test from Remote - master branch.");
+		System.out.println("Pulling Test - remote master branch.");
 	}
 }
