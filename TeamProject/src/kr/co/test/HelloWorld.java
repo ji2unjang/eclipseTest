24c0c09d8b57acd2cfdf86fbe7673b7ef60849a5
jieun <ji2unjang@gmail.com>
2017-03-28 10:04:16 +0900

    second commit from second branch

--- TeamProject/src/kr/co/test/HelloWorld.java
+++ TeamProject/src/kr/co/test/HelloWorld.java
@@ -3,5 +3,6 @@ package kr.co.test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Git World! :)");
+		System.out.println("Hello Git World! :) from second branch! ");
 	}
 }
