53f5d56c5f98889a014072f2e624e6ee39764239
jieun <ji2unjang@gmail.com>
2017-03-28 10:13:48 +0900

    conflict test from master

--- TeamProject/src/kr/co/test/HelloWorld.java
+++ TeamProject/src/kr/co/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Git World! :)");
 		System.out.println("Hello Git World! :) from second branch! ");
+		System.out.println("Conflict Test from Master branch.");
 	}
 }
