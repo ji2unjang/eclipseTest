4939040636e11b71c0853250b5ef1e06c87d28e4
ji2unjang <ji2unjang@gmail.com>
2017-03-28 11:13:29 +0900

    Update SayHello.java
    conflict test

--- TeamProject/src/kr/co/test/SayHello.java
+++ TeamProject/src/kr/co/test/SayHello.java
@@ -3,5 +3,6 @@ package kr.co.test;
 public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("Test for Remote Repository.");
+		System.out.println("Test from Remote - master branch.");
 	}
 }
