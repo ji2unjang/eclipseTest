da4843564a03622806503414bd777e0314296df4
jieun <ji2unjang@gmail.com>
2017-03-28 11:14:58 +0900

    conflict test from local

--- TeamProject/src/kr/co/test/SayHello.java
+++ TeamProject/src/kr/co/test/SayHello.java
@@ -3,5 +3,6 @@ package kr.co.test;
 public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("Test for Remote Repository.");
+		System.out.println("Say Hello from local - master branch.");
 	}
 }
